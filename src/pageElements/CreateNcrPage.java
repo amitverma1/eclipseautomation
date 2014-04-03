911b944b3bfa2666c461d23a62574c664e91c3bf
unknown <Amit.Verma@NDI-LAP-273.3pillar.corp>
2014-04-04 01:07:41 +0530

    Added New class but needs more work on that

---
+++ src/pageElements/CreateNcrPage.java
@@ -0,0 +1,75 @@
+package pageElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CreateNcrPage {
+	
+	public static WebDriver driver_create_ncr;
+	
+	public WebElement titleNCR_field;
+	public WebElement referenceNCR_field;
+	public WebElement descriptionNCR_field;
+	public WebElement revisionNCR_field;
+	public WebElement selectPT_Link;
+	public WebElement createNCR_button;
+	
+	public WebElement error_message_title;
+	public WebElement error_message_reference;
+	public WebElement error_message_description;
+	public WebElement error_message_revision;
+	public WebElement error_message_PTitems;
+	
+	
+	public CreateNcrPage(){
+		
+		titleNCR_field = driver_create_ncr.findElement(By.id("ncrDetailsForm_ncr_titlencrDetailsForm_ncr_title"));
+		referenceNCR_field = driver_create_ncr.findElement(By.id("ncrDetailsForm_ncr_reference"));
+		descriptionNCR_field = driver_create_ncr.findElement(By.id("ncrDescriptionOnDetailsPage"));
+		revisionNCR_field = driver_create_ncr.findElement(By.id("ncrDetailsForm_ncr_revision"));
+	//	selectPT_Link = driver_create_ncr.findElement(By.name("ncr.ptItems"));
+		selectPT_Link = driver_create_ncr.findElement(By.linkText("Select"));
+		createNCR_button = driver_create_ncr.findElement(By.id("ncrDetailsForm_form_button_create"));
+		
+		error_message_title = driver_create_ncr.findElement(By.xpath("//*[@id='wwerr_ncrDetailsForm_ncr_title']/div"));
+		error_message_reference = driver_create_ncr.findElement(By.xpath("//*[@id='wwerr_ncrDetailsForm_ncr_title']/div"));
+		error_message_description = driver_create_ncr.findElement(By.xpath("//*[@id='wwerr_ncrDescriptionOnDetailsPage']/div"));
+		error_message_revision = driver_create_ncr.findElement(By.xpath("//*[@id='wwerr_ncrDetailsForm_ncr_revision']/div"));
+		error_message_PTitems = driver_create_ncr.findElement(By.xpath("//*[@id='entityTypeErrorDiv']"));
+		
+		
+		
+	}
+	
+	
+	public void enter_title(String titleNCR){
+		titleNCR_field.clear();
+		titleNCR_field.sendKeys(titleNCR);
+	}
+	
+	public void enter_reference(String referenceNCR){
+		referenceNCR_field.clear();
+		referenceNCR_field.sendKeys(referenceNCR);
+	}
+	
+	public void enter_decription(String decriptionNCR){
+		descriptionNCR_field.clear();
+		descriptionNCR_field.sendKeys(decriptionNCR);
+	}
+	
+	public void enter_revision(String revisionNCR){
+		revisionNCR_field.clear();
+		revisionNCR_field.sendKeys(revisionNCR);
+	}
+	
+	/*public void enter_title(String titleNCR){
+		titleNCR_field.clear();
+		titleNCR_field.sendKeys(titleNCR);
+	}
+	*/
+	
+	
+	
+
+}
--- src/pageElements/LoginPage.java
+++ src/pageElements/LoginPage.java
@@ -4,21 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+
 public class LoginPage {
 
-	public static WebDriver driver;
+	public static WebDriver driver_login;
 	
 	public WebElement usrnm_field;
 	public WebElement pswrd_field;
 	public WebElement signIn_button;
 	public WebElement error_msg;
 	
-	public LoginPage()
-	{
-		usrnm_field = driver.findElement(By.className("usernameField"));
-		pswrd_field = driver.findElement(By.className("pswrdField"));
-		signIn_button = driver.findElement(By.className("primarySubmitButton"));
-		error_msg = driver.findElement(By.xpath("//div[@class='message error']/p"));
+	public LoginPage()	{
+		
+		usrnm_field = driver_login.findElement(By.className("usernameField"));
+		pswrd_field = driver_login.findElement(By.className("pswrdField"));
+		signIn_button = driver_login.findElement(By.className("primarySubmitButton"));
+		error_msg = driver_login.findElement(By.xpath("//div[@class='message error']/p"));
 	}
 	
 	public void enter_usernm(String uname) {
