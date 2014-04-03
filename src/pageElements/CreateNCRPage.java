a32140349eacd4abd6ad02e6fd22b3fb92f9fb43
unknown <Amit.Verma@NDI-LAP-273.3pillar.corp>
2014-04-03 11:47:13 +0530

    Update-1

--- src/pageElements/CreateNCRPage.java
+++ src/pageElements/CreateNCRPage.java
@@ -1,10 +1,14 @@
 package pageElements;
 
+import org.openqa.selenium.WebDriver;
+
 public class CreateNCRPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+	 WebDriver driver=new WebDriver();
+	
 
-	}
+	
 
 }
