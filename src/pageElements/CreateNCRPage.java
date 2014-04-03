7c13aa0def961575c45283620ee8da4925fdcaae
Saurabh Jain <saurabh.jain1@3pillarglobal.com>
2014-04-03 12:31:21 +0530

    Updating

--- src/pageElements/CreateNCRPage.java
+++ src/pageElements/CreateNCRPage.java
@@ -1,12 +1,13 @@
 package pageElements;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CreateNCRPage {
 
 	
 		
-	 WebDriver driver=new WebDriver();
+	 WebDriver driver=new FirefoxDriver();
 	
 	 
 
