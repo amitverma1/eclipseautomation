054f3122aadaaf3c762c9eed1a29945dc591995d
Saurabh Jain <saurabh.jain1@3pillarglobal.com>
2014-04-03 13:12:20 +0530

    Update

--- src/pageElements/CreateNCRPage.java
+++ src/pageElements/CreateNCRPage.java
@@ -1,13 +1,26 @@
 package pageElements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.FindElement;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CreateNCRPage {
 
+	public static WebDriver driver;
 	
-		
-	 WebDriver driver=new FirefoxDriver();
+	@Before
+	public void beforeclass() {
+		driver = new FirefoxDriver();
+		driver.get("http://ndi-pc-410:8080/josso/signon/login.do?josso_back_to=/ilayout/i-layout");
+	}
 	
+	@Test
+	public void loginWithBlankCredentials()
+	{
+		driver.findElement(By.className("primarySubmitButton")).click();
+	}
 
 }
