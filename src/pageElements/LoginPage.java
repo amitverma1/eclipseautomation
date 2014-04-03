14ad5ab17921887013a728cab324ab675470bd1b
Saurabh Jain <saurabh.jain1@3pillarglobal.com>
2014-04-03 15:34:31 +0530

    Updating

--- src/pageElements/LoginPage.java
+++ src/pageElements/LoginPage.java
@@ -4,13 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class CreateNCRPage {
+public class LoginPage {
 
 	public static WebDriver driver;
 	
